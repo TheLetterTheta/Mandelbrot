205c577df75903d691092c3d12d8d881f45d6203
Nick Dolan <nicholas.dolan1@gmail.com>
2022-04-22 17:35:11 -0500

    Uses Complex data types built in functions

--- src/main.rs
+++ src/main.rs
@@ -1,76 +1,59 @@
 use image::{ImageBuffer, Rgb};
-use rug::Float;
+use palette::{Gradient, LinSrgb};
+use rug::{Complex, Float};
+
+const DOMAIN_PREC: u32 = 20;
+const RESOLUTION_PREC: u32 = 12;
+const TAKE: usize = 100;
+
+const PREC: u32 = DOMAIN_PREC + RESOLUTION_PREC + 10;
 
 struct SquaresComplex {
-    z_real: Float,
-    z_imaginary: Float,
-    c_real: Float,
-    c_imaginary: Float,
-    prec: u32,
+    z: Complex,
+    c: Complex,
 }
 
 impl Iterator for SquaresComplex {
-    type Item = (Float, Float);
+    type Item = ();
 
     fn next(&mut self) -> Option<Self::Item> {
-        let (new_real, new_imaginary) = {
-            let curr_real = &self.z_real;
-            let c_imaginary = &self.c_imaginary;
-            let c_real = &self.c_real;
-            let curr_imaginary = &self.z_imaginary;
+        self.z.square_mut();
+        self.z += &self.c;
 
-            let new_real = Float::with_val(
-                self.prec,
-                Float::with_val(
-                    self.prec,
-                    (curr_real * curr_real) - (curr_imaginary * curr_imaginary),
-                ) + c_real,
-            );
-            let new_imaginary = Float::with_val(
-                self.prec,
-                Float::with_val(4, 2_u8) * curr_real * curr_imaginary + c_imaginary,
-            );
-
-            (new_real, new_imaginary)
-        };
-
-        self.z_real = new_real.clone();
-        self.z_imaginary = new_imaginary.clone();
-
-        if Float::with_val(
-            self.prec,
-            (&self.z_real * &self.z_real) + (&self.z_imaginary * &self.z_imaginary),
-        ) > Float::with_val(4, 4_u8)
-        {
+        let dist = Float::with_val(5, self.z.abs_ref());
+        if dist > Float::with_val(5, 4_f32) {
             None
         } else {
-            Some((new_real, new_imaginary))
+            Some(())
         }
     }
 }
 
-fn square_iter(prec: u32, c_real: Float, c_imaginary: Float) -> SquaresComplex {
+fn square_iter(c: Complex) -> SquaresComplex {
     SquaresComplex {
-        z_real: Float::with_val(4, 0),
-        z_imaginary: Float::with_val(4, 0),
-        c_real,
-        c_imaginary,
-        prec,
+        z: Complex::with_val(PREC, (0_f32, 0_f32)),
+        c,
     }
 }
 
 fn main() {
-    const DOMAIN_PREC: u32 = 3;
-    const RESOLUTION_PREC: u32 = 12;
-    const TAKE: usize = 1000;
+    let domain: [Float; 2] = [
+        Float::with_val(PREC, -0.65_f32),
+        Float::with_val(PREC, -0.55_f32),
+    ];
+    let range: [Float; 2] = [
+        Float::with_val(PREC, -0.55_f32),
+        Float::with_val(PREC, -0.65_f32),
+    ];
 
-    const PREC: u32 = DOMAIN_PREC + RESOLUTION_PREC + 10;
+    const X_RESOLUTION: u32 = 2560;
+    const Y_RESOLUTION: u32 = 1440;
 
-    let domain: [Float; 2] = [Float::with_val(PREC, -1.5_f32), Float::with_val(PREC, 0.5_f32)];
-    let range: [Float; 2] = [Float::with_val(PREC, -1_f32), Float::with_val(PREC, 1_f32)];
-
-    const X_RESOLUTION: u32 = 1440;
-    const Y_RESOLUTION: u32 = 1080;
+    let gradient = Gradient::new([
+        LinSrgb::new(1_f64, 0.5_f64, 0.5_f64),
+        LinSrgb::new(0.5_f64, 1_f64, 0.5_f64),
+        LinSrgb::new(0.5_f64, 0.5_f64, 1_f64),
+    ]);
 
     let x_step = Float::with_val(
         PREC,
@@ -84,13 +67,22 @@ fn main() {
     let img = ImageBuffer::from_fn(X_RESOLUTION, Y_RESOLUTION, |x, y| {
         let x_val = &domain[0] + Float::with_val(PREC, x * &x_step);
         let y_val = &range[0] + Float::with_val(PREC, y * &y_step);
-        let i = square_iter(PREC, x_val, y_val).take(TAKE).count();
+        let i = square_iter(Complex::with_val(PREC, (x_val, y_val)))
+            .take(TAKE)
+            .count();
 
         if i == TAKE {
             Rgb([0, 0, 0])
         } else {
-            let pos = ((i / 1000) * 255) as u8;
-            Rgb([pos / 3, 255, 0])
+            let pos = i as f64 / TAKE as f64;
+            let color = gradient.get(pos);
+            Rgb(unsafe {
+                [
+                    (color.red * 255_f64).to_int_unchecked::<u8>(),
+                    (color.green * 255_f64).to_int_unchecked::<u8>(),
+                    (color.blue * 255_f64).to_int_unchecked::<u8>(),
+                ]
+            })
         }
     });
 
